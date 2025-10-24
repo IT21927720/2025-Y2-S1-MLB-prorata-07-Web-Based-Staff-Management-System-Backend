e3c077229bc3a6b47372a8f2d6672aba4767e2b6
Hasandara Sandamini <hasandarasandamini164@gmail.com>
2025-10-25 00:11:45 +0530

    Update WorkScheduleApplication.java

--- work-schedule/src/main/java/com/staffmanagement/work_schedule/WorkScheduleApplication.java
+++ work-schedule/src/main/java/com/staffmanagement/work_schedule/WorkScheduleApplication.java
@@ -6,6 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WorkScheduleApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(WorkScheduleApplication.class, args)
+		SpringApplication.run(WorkScheduleApplication.class, args);
 	}
 }
